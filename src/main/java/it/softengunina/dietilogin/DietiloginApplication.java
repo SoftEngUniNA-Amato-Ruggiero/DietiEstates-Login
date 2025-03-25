b166f89a6d16b16d23de5ba3ff231287c8006fe0
gyus-e <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2025-03-25 20:14:10 +0100

    First commit

---
+++ src/main/java/it/softengunina/dietilogin/DietiloginApplication.java
@@ -0,0 +1,13 @@
+package it.softengunina.dietilogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietiloginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DietiloginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/softengunina/dietilogin/DietiloginApplicationTests.java
@@ -0,0 +1,13 @@
+package it.softengunina.dietilogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietiloginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
