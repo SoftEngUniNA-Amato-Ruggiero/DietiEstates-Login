d384575c96e56a2d8ad6e052f739c13eee73a6bf
gyus-e <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2025-03-25 21:02:15 +0100

    Added nested comment explaining why contextLoads method is empty

--- src/test/java/it/softengunina/dietilogin/DietiloginApplicationTests.java
+++ src/test/java/it/softengunina/dietilogin/DietiloginApplicationTests.java
@@ -8,6 +8,13 @@ class DietiloginApplicationTests {
 
     @Test
     void contextLoads() {
+        /*
+        This is a basic sanity check to ensure that the application starts up
+        without any issues related to the Spring configuration.
+        This method is empty because the test will pass
+        if the application context loads successfully,
+        and fail if it does not.
+        */
     }
 
 }
